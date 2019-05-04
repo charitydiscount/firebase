26b0220b09c84eca72d0c329e6f73da77a6c4c8d
Lucian Daivdescu <lucian.davidecu@yahoo.ro>
2019-05-04 18:24:13 +0300

    first draft of a function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
-// import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin"
+import * as functions from "firebase-functions"
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+//Function to check the answer submitted
+exports.getUserById = (req, res) => {
+
+    //Extract out the id
+    let id = req.body.id;
+
+    var questionRef = db.collection('useres').doc(id.toString());
+    questionRef.get()
+        .then(doc => {
+            if (!doc.exists) {
+                throw new Error("No such document");
+            } else {
+                res.status(200).send(doc);
+            }
+        })
+        .catch(err => {
+            console.log('Error getting document', err);
+            res.status(400).send('Error');
+        });
+};

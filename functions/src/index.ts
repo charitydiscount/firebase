484052d107a38fb998acde29a6c4c01a6b005df2
Andrei Stefanie <andrei.stefanie@gmail.com>
2019-06-23 18:11:02 +0300

    Updated dependecies and fixed createWallet

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,11 +3,12 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-export const createPointsDocument = functions.auth
+export const createWalletDocument = functions.auth
   .user()
   .onCreate(async (user: functions.auth.UserRecord) => {
     await admin
       .firestore()
+      .collection('points')
       .doc(user.uid)
       .create({
         cashback: {

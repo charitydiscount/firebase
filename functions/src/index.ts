39376c3f5d464cc62cdf5d35fe7fb8e438e4d4d1
Andrei Stefanie <andrei.stefanie@gmail.com>
2019-08-04 20:49:39 +0300

    Function to update user wallet after transaction

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,8 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
+const db = admin.firestore();
+
 export const createWalletDocument = functions.auth
   .user()
   .onCreate(async (user: functions.auth.UserRecord) => {
@@ -20,4 +22,87 @@ export const createWalletDocument = functions.auth
           pending: 0.0
         }
       });
+  });
+
+enum TxType {
+  DONATION = 'DONATION',
+  CASHOUT = 'CASHOUT',
+  BONUS = 'BONUS',
+};
+
+enum TxStatus {
+  ACCEPTED = 'ACCEPTED',
+  REJECTED = 'REJECTED',
+  PENDING = 'PENDING',
+};
+
+export const processTransaction = functions.firestore
+  .document('transactions/{transactionId}')
+  .onCreate(async (snap, context) => {
+    const tx = snap.data();
+    if (!tx) {
+      console.log(`Undefined transaction`);
+      return;
+    }
+    const walletRef = db.doc(`points/${tx.userId}`);
+    const userWallet = await walletRef.get();
+    if (!userWallet || !userWallet.exists) {
+      console.log(`Invalid user ${tx.userId}`);
+      return;
+    }
+
+    const balance = userWallet.data()!.cashback!.approved;
+
+    if (tx.amount > balance) {
+      console.log(`Not enough balance available`);
+      await snap.ref.update({ status: TxStatus.REJECTED });
+      return;
+    }
+
+    const txTimestamp = admin.firestore.Timestamp.fromDate(new Date());
+
+    try {
+      switch (tx.type) {
+        case TxType.DONATION:
+          const meta = await db.doc('meta/2performant').get();
+          const bonusPercentage = meta.data()!.bonusPercentage || 0;
+          await walletRef.update({
+            "cashback.approved": balance - tx.amount,
+            "points.approved": tx.amount * bonusPercentage,
+            "transactions": admin.firestore.FieldValue.arrayUnion(
+              {
+                amount: tx.amount,
+                currency: tx.currency,
+                date: txTimestamp,
+                type: tx.type
+              },
+              {
+                amount: tx.amount * 0.2,
+                currency: tx.currency,
+                date: txTimestamp,
+                type: TxType.BONUS
+              }),
+          });
+          break;
+        case TxType.CASHOUT:
+          await walletRef.update({
+            "cashback.approved": balance - tx.amount,
+            "transactions": admin.firestore.FieldValue.arrayUnion(
+              {
+                amount: tx.amount,
+                currency: tx.currency,
+                date: txTimestamp,
+                type: tx.type
+              })
+          });
+          break;
+        default:
+          break;
+      }
+    } catch (e) {
+      console.log(`Failed to update the user wallet: ${e}`);
+      return;
+    }
+
+    await snap.ref.update({ status: TxStatus.ACCEPTED });
   });

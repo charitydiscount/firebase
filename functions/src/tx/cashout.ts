ea7fe8cabf86f51c7553b71106020f803062cb19
Lucian D <lucian.davidescu@yahoo.ro>
2020-05-23 19:19:10 +0300

    -added settings for admin to save multiple emails for notification about cashouts -integrated notification mails when a cashout is made to alert us -changed text for OTP to RO language

--- functions/src/tx/cashout.ts
+++ functions/src/tx/cashout.ts
@@ -2,17 +2,21 @@ import * as TxDefinitions from './types';
 import { firestore } from 'firebase-admin';
 import elastic from '../elastic';
 import RejectHandler from './reject';
+import { sendEmail } from "../email";
 
 export default class CashoutHandler implements TxDefinitions.TxHandler {
   private walletRef: firestore.DocumentReference;
   private txRef: firestore.DocumentReference;
+  private cashoutEmails: object;
 
   constructor(
     walletRef: firestore.DocumentReference,
     txRef: firestore.DocumentReference,
+    cashoutEmails: object
   ) {
     this.walletRef = walletRef;
     this.txRef = txRef;
+    this.cashoutEmails = cashoutEmails;
   }
 
   async process(
@@ -48,6 +52,28 @@ export default class CashoutHandler implements TxDefinitions.TxHandler {
     await elastic
       .sendBulkRequest(elastic.buildBulkBodyForTx([userTxCashout]))
       .catch((e) => console.log(e));
+
+    //send notification emails
+    if (this.cashoutEmails && this.cashoutEmails.toString().length > 0) {
+      try {
+        const body = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <title>New Cashout</title>
+        </head>
+        <body>
+            <p>A new cashout was initiated, please visit admin page to view more info</p>
+        </body>
+        </html>
+        `;
+        await sendEmail(this.cashoutEmails.toString(), 'New Cashout', body);
+      } catch (e) {
+        console.log(e);
+      }
+    }
 
     return { status: TxDefinitions.TxStatus.ACCEPTED };
   }

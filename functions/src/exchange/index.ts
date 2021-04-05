60e74c478f92c5a75b55e16abbf2e415438ad89d
Andrei Stefanie <Andrei.Stefanie@msg.group>
2021-04-05 17:04:27 +0300

    Use BNR for exchange rates

--- functions/src/exchange/index.ts
+++ functions/src/exchange/index.ts
@@ -1,5 +1,6 @@
 import { config } from 'firebase-functions';
 import fetch = require('node-fetch');
+import { parseStringPromise } from 'xml2js';
 
 export const BASE_CURRENCY = 'RON';
 
@@ -8,44 +9,27 @@ export interface ConvertedAmount {
   currency: string;
 }
 
-interface ExchangeRates {
-  base: string;
-  date: Date;
-  rates: {
-    [currencyCode: string]: number;
-  };
-}
-
-let exchangeRates: ExchangeRates;
-
 export async function convertAmount(
   amount: number,
   currency: string,
   targetCurrency: string = BASE_CURRENCY,
 ): Promise<ConvertedAmount> {
-  // Get the exchange rates
-  if (exchangeRates === undefined || exchangeRates.base !== targetCurrency) {
-    const exchangeResponse = await fetch.default(
-      `https://api.exchangeratesapi.io/latest?base=${targetCurrency}`,
-    );
-    if (exchangeResponse.status !== 200) {
-      console.log(
-        `Failed to get the exchange rates: ${exchangeResponse.statusText}`,
-      );
-      // Return the provided amount and use it as it is
-      return {
-        amount,
-        currency,
-      };
-    }
+  let targetAmount = amount;
 
-    exchangeRates = await exchangeResponse.json();
+  const officialRate = await getRate(currency);
+  if (!officialRate) {
+    console.log(`Could not fetch the official rate`);
+    // Return the provided amount and use it as it is
+    return {
+      amount,
+      currency,
+    };
   }
 
-  let targetAmount = amount;
-  const rate =
-    exchangeRates.rates[currency] +
-    (exchangeRates.rates[currency] * config().exchange.fee || 0.02);
+  const fee = config().exchange.fee || 0.02;
+
+  const rate = officialRate + officialRate * fee;
+
   try {
     targetAmount = amount / rate;
   } catch (error) {
@@ -62,6 +46,21 @@ export async function convertAmount(
     currency: targetCurrency,
   };
 }
+
+const getRate = async (currency: string): Promise<number | undefined> => {
+  const bnrResponse = await fetch.default('https://www.bnr.ro/nbrfxrates.xml');
+  if (bnrResponse.status !== 200) {
+    console.error(`Failed to fetch the BNR rates: ${bnrResponse.statusText}`);
+    return;
+  }
+
+  const xml = await bnrResponse.text();
+  const parsedXML = await parseStringPromise(xml);
+  const rates = parsedXML.DataSet.Body[0].Cube[0].Rate as any[];
+  const rate = rates.find((rate: any) => rate['$'].currency === currency);
+
+  return rate ? rate['_'] : undefined;
+};
 
 export const roundAmount = (amount: number) => {
   return Math.round((amount + Number.EPSILON) * 100) / 100;
